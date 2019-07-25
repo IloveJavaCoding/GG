8aca5ca0e0a0bf6a7cffe7f86057a5faa9485c16
diya <43806216+diya@users.noreply.github.com>
2019-07-26 03:25:24 +0800

    。

--- GGNews.Designer.cs
+++ GGNews.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.messageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contactsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chinaNewsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gGNewsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.messageToolStripMenuItem,
+            this.contactsToolStripMenuItem,
+            this.newsToolStripMenuItem,
+            this.userToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // messageToolStripMenuItem
+            // 
+            this.messageToolStripMenuItem.Name = "messageToolStripMenuItem";
+            this.messageToolStripMenuItem.Size = new System.Drawing.Size(73, 21);
+            this.messageToolStripMenuItem.Text = "Message";
+            this.messageToolStripMenuItem.Click += new System.EventHandler(this.MessageToolStripMenuItem_Click);
+            // 
+            // contactsToolStripMenuItem
+            // 
+            this.contactsToolStripMenuItem.Name = "contactsToolStripMenuItem";
+            this.contactsToolStripMenuItem.Size = new System.Drawing.Size(70, 21);
+            this.contactsToolStripMenuItem.Text = "Contacts";
+            this.contactsToolStripMenuItem.Click += new System.EventHandler(this.ContactsToolStripMenuItem_Click);
+            // 
+            // newsToolStripMenuItem
+            // 
+            this.newsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.chinaNewsToolStripMenuItem,
+            this.gGNewsToolStripMenuItem});
+            this.newsToolStripMenuItem.Name = "newsToolStripMenuItem";
+            this.newsToolStripMenuItem.Size = new System.Drawing.Size(52, 21);
+            this.newsToolStripMenuItem.Text = "News";
+            // 
+            // userToolStripMenuItem
+            // 
+            this.userToolStripMenuItem.Name = "userToolStripMenuItem";
+            this.userToolStripMenuItem.Size = new System.Drawing.Size(47, 21);
+            this.userToolStripMenuItem.Text = "User";
+            this.userToolStripMenuItem.Click += new System.EventHandler(this.UserToolStripMenuItem_Click);
+            // 
+            // chinaNewsToolStripMenuItem
+            // 
+            this.chinaNewsToolStripMenuItem.Name = "chinaNewsToolStripMenuItem";
+            this.chinaNewsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.chinaNewsToolStripMenuItem.Text = "China News";
+            this.chinaNewsToolStripMenuItem.Click += new System.EventHandler(this.ChinaNewsToolStripMenuItem_Click);
+            // 
+            // gGNewsToolStripMenuItem
+            // 
+            this.gGNewsToolStripMenuItem.Name = "gGNewsToolStripMenuItem";
+            this.gGNewsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.gGNewsToolStripMenuItem.Text = "GG News";
+            this.gGNewsToolStripMenuItem.Click += new System.EventHandler(this.GGNewsToolStripMenuItem_Click);
+            // 
+            // GGNews
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "GGNews";
             this.Text = "GGNews";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem messageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contactsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem userToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chinaNewsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gGNewsToolStripMenuItem;
     }
 }
--- GGNews.cs
+++ GGNews.cs
@@ -18,5 +18,51 @@ namespace GG
             InitializeComponent();
             this.username = username;
         }
+
+        private void MessageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Hide();
+            Homepage homepage = new Homepage(username)
+            {
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            homepage.Show();
+        }
+
+        private void UserToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Hide();
+            User user = new User(username)
+            {
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            user.Show();
+        }
+
+        private void ContactsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Hide();
+            Contact contact = new Contact(username);
+            contact.StartPosition = FormStartPosition.CenterScreen;
+            contact.Show();
+        }
+
+        private void ChinaNewsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Hide();
+            News news = new News(username)
+            {
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            news.Show();
+        }
+
+        private void GGNewsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Hide();
+            GGNews GGnews = new GGNews(username);
+            GGnews.StartPosition = FormStartPosition.CenterScreen;
+            GGnews.Show();
+        }
     }
 }
